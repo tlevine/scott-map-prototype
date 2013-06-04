4eabb8116b0b502d5e3dfba22424c192d24d5868
Thomas Levine <occurrence@thomaslevine.com>
2013-06-04 10:41:00 -0700

    make a box of span6 size

--- js.js
+++ js.js
@@ -6,11 +6,11 @@ function createMap(){
   var xy = d3.geo.albersUsa();
   var svg = d3.select("#viz").append("svg");
   svg.attr("id", "mainSVG")
-    .attr("width", 200)
-    .attr("height", 200)
+    .attr("width", 940)
+    .attr("height", 940)
 
   var perishes = svg.append("g")
-    .attr("transform", function(d) { return "scale(2)"})
+    .attr("transform", function(d) { return "scale(5)"})
     .append('g')
       .attr("id", "perishes")
       .attr("transform", function(d) { return "translate(-500, -300)"})
