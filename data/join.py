402479fc3b6f7b0a414d42246ae1f42a6ce4d397
Thomas Levine <occurrence@thomaslevine.com>
2013-08-16 11:10:55 -0700

    joining scott data to the thing

---
+++ data/join.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import json
+from dumptruck import DumpTruck
+dt = DumpTruck(dbname = 'applications.db')
+
+def scott_data():
+    sql = '''
+    SELECT "parish", sum("acreage") AS 'acreage'
+    FROM application
+    WHERE "type" = 'impact' AND "parish" != ''
+    GROUP BY "parish";
+    '''
+
+    return {row['parish'].upper().replace('SAINT', 'ST'): row['acreage'] for row in dt.execute(sql)}
+
+scott = scott_data()
+
+parishes = json.load(open('parishes.json'))
+counties_parishes = [feature['properties']['COUNTY'] for feature in parishes['features']]
